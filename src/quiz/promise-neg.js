1d3968d734589a309d32a48e4c0f16e4ce75915c
Snehitha Barukula <barukula.s@northeastern.edu>
2024-03-04 11:10:44 -0500

    Class Activity

--- src/quiz/promise-neg.js
+++ src/quiz/promise-neg.js
@@ -3,3 +3,28 @@ const array2D = [
     [4, 5, 6],
     [7, 8, -9]
 ];
+
+function negativeArr(arr, row) {
+    return new Promise((resolve, reject) => {
+        if (arr.length > row) {
+            const negs = arr[row].filter(n => n<0);
+            if (negs.length > 0 ) {
+                resolve(arr[row]);
+            }
+            else {
+                reject("no negatives in this row");
+            }
+        }
+        else {
+            reject("Invalid row number");
+        }
+    })
+}
+
+rows = []
+
+for(let x=0;x<array2D.length;x++) {
+    rows.push(negativeArr(array2D, x));
+}
+
+Promise.any(rows).then( (res) => console.log(res));
--- src/quiz/promise-sum-2darray.js
+++ src/quiz/promise-sum-2darray.js
@@ -3,3 +3,36 @@ const array2D = [
     [4, 5, 6],
     [7, 8, 9]
 ];
+
+function rowSum( arr, row) {
+    return new Promise((resolve, reject) => {
+        if (arr.length > row) {
+            setTimeout(() => {
+                let sum = 0;
+                for (let i=0;i<arr.length;i++) {
+                    sum+= arr[row][i];
+                }
+                resolve(sum);
+            }, 0);
+        }
+        else {
+            reject(`Row ${row} not within 0 and ${arr.length}`);
+        }
+    })
+}
+
+promisesArr = []
+
+for(let x =0; x<array2D.length; x++) {
+    promisesArr.push(rowSum(array2D, x));
+}
+
+x = new Date()
+Promise.all(promisesArr)
+    .then((sums) => {
+        let sum = 0;
+        sums.forEach(s => sum+= s);
+        console.log(sum);
+        console.log(new Date() - x)
+    })
+.catch(error => console.log(error));
